012bceccc23272ea781018ff5e576f259cde6cb0
Rafael Pereira <rafahenriquee13@yahoo.com.br>
2022-05-06 23:08:12 -0300

    REST pattern and add custom Status Code

--- Program.cs
+++ Program.cs
@@ -5,23 +5,28 @@ var app = builder.Build();
 
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
+    return Results.Created($"/products/{product.Code}", product.Code);
 });
 
 //api.app.com/user/{code} = Route Params
 app.MapGet("/products/{code}", ([FromRoute] string code) => {
     var product = ProductRepository.GetBy(code);
-    return product;
-});
+    if (product != null)
+        return Results.Ok(product);
 
+    return Results.NotFound();
+});
 
 app.MapPut("/products", (Product product) => {
     var productSaved = ProductRepository.GetBy(product.Code);
     productSaved.Name = product.Name;
+    return Results.Ok();
 });
 
 app.MapDelete("/products/{code}", ([FromRoute] string code) => {
     var productSaved = ProductRepository.GetBy(code);
     ProductRepository.Remove(productSaved);
+    return Results.Ok();
 });
 
 app.Run();
