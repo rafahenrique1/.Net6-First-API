cfbdd99b9cf236fd960241c6d9e5202b1ff87be8
Rafael Pereira <rafahenriquee13@yahoo.com.br>
2022-05-11 16:30:10 -0300

    Add EntityFramework Packages and Config File

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
@@ -29,17 +31,22 @@ app.MapDelete("/products/{code}", ([FromRoute] string code) => {
     return Results.Ok();
 });
 
+app.MapGet("/configuration/database", (IConfiguration configuration) => {
+    return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
+
 app.Run();
 
 public static class ProductRepository{
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = new List<Product>();
+
+    public static void Init(IConfiguration configuration){
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
 
     public static void Add(Product product){
-        if (Products == null){
-            Products = new List<Product>();
-
             Products.Add(product);
-        }
     }
 
     public static Product GetBy(string code){
@@ -51,7 +58,7 @@ public static class ProductRepository{
     }
 }
 
-public class Product{
-    public string Code { get; set; }
-    public string Name { get; set; }
+    public class Product{
+         public string Code { get; set; }
+         public string Name { get; set; }
 }
